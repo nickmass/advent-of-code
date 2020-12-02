c5e75c11a09fce3367c5dc41a0fe5f75c38e804c
Nick Massey <nickmass@nickmass.com>
2020-12-02 08:20:14 -0700

    Complete up to day 2

---
+++ src/main.rs
@@ -0,0 +1,204 @@
+use std::collections::HashSet;
+
+fn main() {
+    println!("Day One a: {}", problem_one_a(PROBLEM_ONE));
+    println!("Day One b: {}", problem_one_b(PROBLEM_ONE));
+    println!("Day Two a: {}", problem_two_a(PROBLEM_TWO));
+    println!("Day Two b: {}", problem_two_b(PROBLEM_TWO));
+}
+
+const PROBLEM_ONE: &'static str = include_str!("problems/day1.txt");
+const PROBLEM_TWO: &'static str = include_str!("problems/day2.txt");
+
+fn problem_one_a(input: &'static str) -> u64 {
+    let values: HashSet<u64> = input
+        .lines()
+        .map(str::parse)
+        .filter_map(Result::ok)
+        .collect();
+
+    for n in &values {
+        let x = 2020 - n;
+        if values.contains(&x) {
+            return x * n;
+        }
+    }
+
+    0
+}
+
+fn problem_one_b(input: &'static str) -> u64 {
+    let values: HashSet<u64> = input
+        .lines()
+        .map(str::parse)
+        .filter_map(Result::ok)
+        .collect();
+
+    for a in &values {
+        for b in &values {
+            if a + b > 2020 {
+                continue;
+            }
+            let x = 2020 - a - b;
+            if values.contains(&x) {
+                return x * a * b;
+            }
+        }
+    }
+
+    0
+}
+
+#[derive(Debug)]
+struct PwLine<'a> {
+    min: usize,
+    max: usize,
+    character: char,
+    password: &'a str,
+}
+
+impl<'a> PwLine<'a> {
+    fn from_str(s: &'a str) -> Option<Self> {
+        let (s, min) = take_u64(s)?;
+        let s = take_token(s, "-")?;
+        let (s, max) = take_u64(s)?;
+        let s = take_token(s, " ")?;
+        let (s, character) = take_char(s)?;
+        let s = take_token(s, ": ")?;
+        let (_s, password) = take_until(s, |_| false)?;
+
+        Some(PwLine {
+            min: min as usize,
+            max: max as usize,
+            character,
+            password,
+        })
+    }
+}
+
+fn problem_two_a(input: &'static str) -> u64 {
+    let lines: Vec<_> = input.lines().flat_map(PwLine::from_str).collect();
+
+    let mut valid = 0;
+    for pw in lines {
+        let count = pw.password.chars().fold(0, |state, next| {
+            if next == pw.character {
+                state + 1
+            } else {
+                state
+            }
+        });
+
+        if count >= pw.min && count <= pw.max {
+            valid += 1;
+        }
+    }
+
+    valid
+}
+
+fn problem_two_b(input: &'static str) -> u64 {
+    let lines: Vec<_> = input.lines().flat_map(PwLine::from_str).collect();
+
+    let mut valid = 0;
+    for pw in lines {
+        let mut found = false;
+        for (idx, is_match) in pw
+            .password
+            .char_indices()
+            .map(|(idx, c)| (idx, c == pw.character))
+        {
+            if idx == pw.min - 1 && is_match {
+                found = true;
+            } else if idx == pw.max - 1 {
+                if found && !is_match || !found && is_match {
+                    valid += 1;
+                }
+                break;
+            }
+        }
+    }
+
+    valid
+}
+
+fn take_u64(s: &str) -> Option<(&str, u64)> {
+    let mut index = 0;
+    for c in s.chars() {
+        if c.is_digit(10) {
+            index += 1;
+        } else {
+            break;
+        }
+    }
+
+    if index == 0 {
+        None
+    } else {
+        let n = s[0..index].parse().ok();
+        let remainder = &s[index..];
+        n.map(|n| (remainder, n))
+    }
+}
+
+fn take_token<'a, 'b>(s: &'a str, n: &'b str) -> Option<&'a str> {
+    if n.len() > s.len() {
+        None
+    } else {
+        let search = &s[0..n.len()];
+        if search == n {
+            Some(&s[n.len()..])
+        } else {
+            None
+        }
+    }
+}
+
+fn take_char(s: &str) -> Option<(&str, char)> {
+    if s.len() > 0 {
+        let c = s.chars().next().unwrap();
+        Some((&s[1..], c))
+    } else {
+        None
+    }
+}
+
+fn take_until<F: Fn(char) -> bool>(s: &str, f: F) -> Option<(&str, &str)> {
+    let mut index = 0;
+    for c in s.bytes() {
+        if f(c as char) {
+            break;
+        }
+        index += 1;
+    }
+
+    let (n, s) = s.split_at(index);
+    Some((s, n))
+}
+
+#[test]
+fn parse_test() {
+    let s = "123abc";
+    let (s, num) = take_u64(s).unwrap();
+    assert_eq!(num, 123);
+    assert_eq!(s, "abc");
+
+    let s = "abc123";
+    let n = take_u64(s);
+    assert!(n.is_none());
+
+    let s = "abc123";
+    let s = take_token(s, "abc").unwrap();
+    let (s, num) = take_u64(s).unwrap();
+    assert_eq!(s, "");
+    assert_eq!(num, 123);
+
+    let s = "abc123";
+    let s = take_token(s, "abc123efg");
+    assert!(s.is_none());
+
+    let s = "abc123";
+    let (s, n) = take_until(s, |c| c.is_digit(10)).unwrap();
+    assert_eq!(s, "123");
+    assert_eq!(n, "abc");
+}
