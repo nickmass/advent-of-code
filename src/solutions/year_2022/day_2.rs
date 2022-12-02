178875a68513ce9c217d0662525014ed126584ef
Nick Massey <nickmass@nickmass.com>
2022-12-02 10:32:22 -0700

    Clean up day 2

--- src/solutions/year_2022/day_2.rs
+++ src/solutions/year_2022/day_2.rs
@@ -2,16 +2,16 @@ pub fn part_one(input: &str) -> u64 {
     input
         .trim()
         .lines()
-        .map(|l| match l.split_once(" ").unwrap() {
-            ("A", "X") => 4,
-            ("A", "Y") => 8,
-            ("A", "Z") => 3,
-            ("B", "X") => 1,
-            ("B", "Y") => 5,
-            ("B", "Z") => 9,
-            ("C", "X") => 7,
-            ("C", "Y") => 2,
-            ("C", "Z") => 6,
+        .map(|l| match l {
+            "A X" => 4,
+            "A Y" => 8,
+            "A Z" => 3,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 7,
+            "C Y" => 2,
+            "C Z" => 6,
             _ => unreachable!(),
         })
         .sum()
@@ -21,16 +21,16 @@ pub fn part_two(input: &str) -> u64 {
     input
         .trim()
         .lines()
-        .map(|l| match l.split_once(" ").unwrap() {
-            ("A", "X") => 3,
-            ("A", "Y") => 4,
-            ("A", "Z") => 8,
-            ("B", "X") => 1,
-            ("B", "Y") => 5,
-            ("B", "Z") => 9,
-            ("C", "X") => 2,
-            ("C", "Y") => 6,
-            ("C", "Z") => 7,
+        .map(|l| match l {
+            "A X" => 3,
+            "A Y" => 4,
+            "A Z" => 8,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 2,
+            "C Y" => 6,
+            "C Z" => 7,
             _ => unreachable!(),
         })
         .sum()
