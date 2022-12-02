7f43f0bac7e030d881ddf5cf3596a22525f9f789
Nick Massey <nickmass@nickmass.com>
2022-12-01 22:24:05 -0700

    2022 - Day 2

--- src/solutions/year_2022.rs
+++ src/solutions/year_2022.rs
@@ -1,7 +1,11 @@
 use crate::{solution, Solution};
 
 mod day_1;
+mod day_2;
 
 pub fn days() -> Vec<Solution> {
-    vec![solution!(1, day_1::part_one, day_1::part_two)]
+    vec![
+        solution!(1, day_1::part_one, day_1::part_two),
+        solution!(2, day_2::part_one, day_2::part_two),
+    ]
 }
