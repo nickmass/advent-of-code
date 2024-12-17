b033029657694475a04f7202e26506ae6798afd7
Nick Massey <nickmass@nickmass.com>
2024-12-17 13:19:22 -0700

    Much improved 17 part 2

--- src/solutions/year_2024/day_17.rs
+++ src/solutions/year_2024/day_17.rs
@@ -1,118 +1,85 @@
+use std::collections::BinaryHeap;
+
 pub fn part_one(input: &str) -> String {
-    Program::new(input).part_one()
+    let (mut machine, program) = parse(input);
+
+    let mut output = String::new();
+    while let Interrupt::Output(b) = machine.run(&program) {
+        let c = (b + b'0') as char;
+        if output.len() > 0 {
+            output.push(',');
+        }
+        output.push(c);
+    }
+
+    output
 }
 
 pub fn part_two(input: &str) -> u64 {
-    Program::new(input).part_two()
-}
+    let (machine, program) = parse(input);
 
-struct Program {
-    init_a: u64,
-    init_b: u64,
-    init_c: u64,
+    let mut output = Vec::new();
+    let mut heap = BinaryHeap::new();
+    heap.push(Search(0, 0));
 
-    mask_a: u64,
-    bits_a: u64,
-    inc_a: u64,
-    max_out: usize,
+    while let Some(Search(base_a, valid)) = heap.pop() {
+        'outer: for n in 0..0x80 {
+            let mut machine = machine.clone();
+            output.clear();
 
-    a: u64,
-    b: u64,
-    c: u64,
+            let a = n << (valid * 3) | base_a;
+            machine.a = a;
 
-    pc: usize,
-    program: Vec<u8>,
-    output: Vec<u8>,
-}
+            while let Interrupt::Output(b) = machine.run(&program) {
+                if output.len() < program.len() && b == program[output.len()] {
+                    output.push(b);
 
-impl Program {
-    fn new(input: &str) -> Self {
-        let mut program = Vec::new();
-        let mut a = 0;
-        let mut b = 0;
-        let mut c = 0;
-
-        for line in input.lines() {
-            let line = line.trim();
-            if line.is_empty() {
-                continue;
+                    if output.len() > valid {
+                        let valid = output.len();
+                        let mask = (1 << (valid * 3)) - 1;
+                        heap.push(Search(a & mask, valid));
+                    }
+                } else {
+                    continue 'outer;
+                }
             }
 
-            if line.starts_with("Register A: ") {
-                let (_, a_s) = line.split_once(": ").unwrap();
-                a = a_s.parse().unwrap();
-            } else if line.starts_with("Register B: ") {
-                let (_, b_s) = line.split_once(": ").unwrap();
-                b = b_s.parse().unwrap();
-            } else if line.starts_with("Register C: ") {
-                let (_, c_s) = line.split_once(": ").unwrap();
-                c = c_s.parse().unwrap();
-            } else if line.starts_with("Program: ") {
-                let ops = line
-                    .get("Program: ".len()..)
-                    .into_iter()
-                    .flat_map(|p| p.split(","))
-                    .map(|n| n.parse::<u8>().unwrap());
-                program.extend(ops);
+            if output.len() == program.len() {
+                return a;
             }
-        }
-
-        Self {
-            a,
-            b,
-            c,
-
-            init_a: a,
-            init_b: b,
-            init_c: c,
-
-            inc_a: 0,
-            mask_a: 0,
-            bits_a: 0,
-            max_out: 4,
-
-            pc: 0,
-            program,
-            output: Vec::new(),
         }
     }
 
-    fn run(&mut self, part_two: bool) {
+    0
+}
+
+#[derive(Debug, Clone)]
+struct Machine {
+    a: u64,
+    b: u64,
+    c: u64,
+    pc: usize,
+}
+
+impl Machine {
+    fn run(&mut self, mem: &[u8]) -> Interrupt {
         loop {
-            let Some((opcode, operand)) = self.read().zip(self.read()) else {
-                if part_two {
-                    if self.output.len() == self.program.len() {
-                        break;
-                    } else {
-                        self.next_a();
-                        continue;
-                    }
-                } else {
-                    break;
-                }
+            let Some((opcode, operand)) = self.read_instruction(mem) else {
+                return Interrupt::Halt;
             };
             let combo = self.combo(operand);
-            let literal = operand as u64;
 
             match opcode & 7 {
                 0 => self.a = self.a >> combo,
-                1 => self.b ^= literal,
+                1 => self.b ^= operand as u64,
                 2 => self.b = combo & 7,
                 3 => {
                     if self.a != 0 {
-                        self.pc = literal as usize;
+                        self.pc = operand as usize;
                     }
                 }
                 4 => self.b ^= self.c,
-                5 if part_two => {
-                    let v = (combo & 7) as u8;
-                    if self.program.get(self.output.len()).copied() == Some(v) {
-                        self.output.push(v);
-                    } else {
-                        self.next_a();
-                    }
-                }
-                5 => self.output.push((combo & 7) as u8),
+                5 => return Interrupt::Output((combo & 7) as u8),
                 6 => self.b = self.a >> combo,
                 7 => self.c = self.a >> combo,
                 _ => unreachable!(),
@@ -130,92 +97,84 @@ impl Program {
         }
     }
 
-    fn read(&mut self) -> Option<u8> {
-        if self.pc >= self.program.len() {
+    fn read_instruction(&mut self, mem: &[u8]) -> Option<(u8, u8)> {
+        self.read(mem).zip(self.read(mem))
+    }
+
+    fn read(&mut self, mem: &[u8]) -> Option<u8> {
+        if self.pc >= mem.len() {
             None
         } else {
-            let v = self.program[self.pc];
+            let v = mem[self.pc];
             self.pc += 1;
             Some(v)
         }
     }
+}
 
-    fn reset(&mut self) {
-        self.pc = 0;
-        self.a = self.init_a;
-        self.b = self.init_b;
-        self.c = self.init_c;
-        self.output.clear();
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+struct Search(u64, usize);
+
+impl std::cmp::Ord for Search {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self.1.cmp(&other.1) {
+            std::cmp::Ordering::Equal => other.0.cmp(&self.0),
+            c => c,
+        }
     }
+}
 
-    fn next_a(&mut self) {
-        if self.output.len() > self.max_out + 1 {
-            let v = self.inc_a << self.bits_a | self.mask_a;
-            self.bits_a = self.output.len() as u64 * 3;
-            let mask = (1 << self.bits_a) - 1;
-            self.mask_a = v & mask;
-            self.max_out = self.output.len();
+impl std::cmp::PartialOrd for Search {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn parse(input: &str) -> (Machine, Vec<u8>) {
+    let mut program = Vec::new();
+    let mut a = 0;
+    let mut b = 0;
+    let mut c = 0;
+
+    for line in input.lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
         }
 
-        self.reset();
-        self.inc_a += 1;
-        self.a = self.inc_a << self.bits_a | self.mask_a;
+        if let Some(s) = take_token("Register A: ", line) {
+            a = s.parse().unwrap();
+        } else if let Some(s) = take_token("Register B: ", line) {
+            b = s.parse().unwrap();
+        } else if let Some(s) = take_token("Register C: ", line) {
+            c = s.parse().unwrap();
+        } else if let Some(s) = take_token("Program: ", line) {
+            let ops = s.split(",").map(|n| n.parse::<u8>().unwrap());
+            program.extend(ops);
+        }
     }
 
-    fn part_one(&mut self) -> String {
-        self.run(false);
+    let machine = Machine { a, b, c, pc: 0 };
 
-        let mut output: String = self
-            .output
-            .iter()
-            .flat_map(|&n| [(n + b'0') as char, ','])
-            .collect();
-        output.pop();
-        output
+    (machine, program)
+}
+
+fn take_token<'a>(token: &str, s: &'a str) -> Option<&'a str> {
+    if s.starts_with(token) {
+        Some(&s[token.len()..])
+    } else {
+        None
     }
+}
 
-    fn part_two(&mut self) -> u64 {
-        self.a = 0;
-        self.run(true);
-
-        self.inc_a << self.bits_a | self.mask_a
-    }
+#[derive(Debug, Copy, Clone)]
+enum Interrupt {
+    Halt,
+    Output(u8),
 }
 
 #[test]
 fn test() {
-    let mut program = Program::new("");
-    program.c = 9;
-    program.program.extend([2, 6]);
-    program.run(false);
-    assert_eq!(program.b, 1);
-
-    let mut program = Program::new("");
-    program.a = 10;
-    program.program.extend([5, 0, 5, 1, 5, 4]);
-    program.run(false);
-    assert_eq!(program.part_one(), "0,1,2");
-
-    let mut program = Program::new("");
-    program.a = 2024;
-    program.program.extend([0, 1, 5, 4, 3, 0]);
-    program.run(false);
-    assert_eq!(program.part_one(), "4,2,5,6,7,7,7,7,3,1,0");
-    assert_eq!(program.a, 0);
-
-    let mut program = Program::new("");
-    program.b = 29;
-    program.program.extend([1, 7]);
-    program.run(false);
-    assert_eq!(program.b, 26);
-
-    let mut program = Program::new("");
-    program.b = 2024;
-    program.c = 43690;
-    program.program.extend([4, 0]);
-    program.run(false);
-    assert_eq!(program.b, 44354);
-
     let input = r#"Register A: 729
 Register B: 0
 Register C: 0
